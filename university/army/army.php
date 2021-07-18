09b604273bcfeac66faaed1131c72b10026755ec
Kishan Nayak <35322004+kishannk11@users.noreply.github.com>
2021-07-18 19:05:53 +0530

    Update army.php

--- university/army/army.php
+++ university/army/army.php
@@ -45,186 +45,6 @@
         </div>
     </div>
     <!-- Start Rightbar setting panel -->
-    <!--<div id="rightsidebar" class="right_sidebar">
-        <a href="javascript:void(0)" class="p-3 settingbar float-right"><i class="fa fa-close"></i></a>
-        <ul class="nav nav-tabs" role="tablist">
-            <li class="nav-item"><a class="nav-link active" data-toggle="tab" href="#Settings" aria-expanded="true">Settings</a></li>
-            <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#activity" aria-expanded="false">Activity</a></li>
-        </ul>
-        <div class="tab-content">
-            <div role="tabpanel" class="tab-pane vivify fadeIn active" id="Settings" aria-expanded="true">
-                <div class="mb-4">
-                    <h6 class="font-14 font-weight-bold text-muted">Theme Color</h6>
-                    <ul class="choose-skin list-unstyled mb-0">
-                        <li data-theme="azure"><div class="azure"></div></li>
-                        <li data-theme="indigo"><div class="indigo"></div></li>
-                        <li data-theme="purple"><div class="purple"></div></li>
-                        <li data-theme="orange"><div class="orange"></div></li>
-                        <li data-theme="green"><div class="green"></div></li>
-                        <li data-theme="cyan" class="active"><div class="cyan"></div></li>
-                        <li data-theme="blush"><div class="blush"></div></li>
-                        <li data-theme="white"><div class="bg-white"></div></li>
-                    </ul>
-                </div>
-                <div class="mb-4">
-                    <h6 class="font-14 font-weight-bold text-muted">Font Style</h6>
-                    <div class="custom-controls-stacked font_setting">
-                        <label class="custom-control custom-radio custom-control-inline">
-                            <input type="radio" class="custom-control-input" name="font" value="font-muli" checked="">
-                            <span class="custom-control-label">Muli Google Font</span>
-                        </label>
-                        <label class="custom-control custom-radio custom-control-inline">
-                            <input type="radio" class="custom-control-input" name="font" value="font-montserrat">
-                            <span class="custom-control-label">Montserrat Google Font</span>
-                        </label>
-                        <label class="custom-control custom-radio custom-control-inline">
-                            <input type="radio" class="custom-control-input" name="font" value="font-poppins">
-                            <span class="custom-control-label">Poppins Google Font</span>
-                        </label>
-                    </div>
-                </div>
-                <div>
-                    <h6 class="font-14 font-weight-bold mt-4 text-muted">General Settings</h6>
-                    <ul class="setting-list list-unstyled mt-1 setting_switch">
-                        <li>
-                            <label class="custom-switch">
-                                <span class="custom-switch-description">Night Mode</span>
-                                <input type="checkbox" name="custom-switch-checkbox" class="custom-switch-input btn-darkmode">
-                                <span class="custom-switch-indicator"></span>
-                            </label>
-                        </li>
-                        <li>
-                            <label class="custom-switch">
-                                <span class="custom-switch-description">Fix Navbar top</span>
-                                <input type="checkbox" name="custom-switch-checkbox" class="custom-switch-input btn-fixnavbar">
-                                <span class="custom-switch-indicator"></span>
-                            </label>
-                        </li>
-                        <li>
-                            <label class="custom-switch">
-                                <span class="custom-switch-description">Header Dark</span>
-                                <input type="checkbox" name="custom-switch-checkbox" class="custom-switch-input btn-pageheader">
-                                <span class="custom-switch-indicator"></span>
-                            </label>
-                        </li>
-                        <li>
-                            <label class="custom-switch">
-                                <span class="custom-switch-description">Min Sidebar Dark</span>
-                                <input type="checkbox" name="custom-switch-checkbox" class="custom-switch-input btn-min_sidebar">
-                                <span class="custom-switch-indicator"></span>
-                            </label>
-                        </li>
-                        <li>
-                            <label class="custom-switch">
-                                <span class="custom-switch-description">Sidebar Dark</span>
-                                <input type="checkbox" name="custom-switch-checkbox" class="custom-switch-input btn-sidebar">
-                                <span class="custom-switch-indicator"></span>
-                            </label>
-                        </li>
-                        <li>
-                            <label class="custom-switch">
-                                <span class="custom-switch-description">Icon Color</span>
-                                <input type="checkbox" name="custom-switch-checkbox" class="custom-switch-input btn-iconcolor">
-                                <span class="custom-switch-indicator"></span>
-                            </label>
-                        </li>
-                        <li>
-                            <label class="custom-switch">
-                                <span class="custom-switch-description">Gradient Color</span>
-                                <input type="checkbox" name="custom-switch-checkbox" class="custom-switch-input btn-gradient" checked="">
-                                <span class="custom-switch-indicator"></span>
-                            </label>
-                        </li>
-                        <li>
-                            <label class="custom-switch">
-                                <span class="custom-switch-description">Box Shadow</span>
-                                <input type="checkbox" name="custom-switch-checkbox" class="custom-switch-input btn-boxshadow">
-                                <span class="custom-switch-indicator"></span>
-                            </label>
-                        </li>
-                        <li>
-                            <label class="custom-switch">
-                                <span class="custom-switch-description">RTL Support</span>
-                                <input type="checkbox" name="custom-switch-checkbox" class="custom-switch-input btn-rtl">
-                                <span class="custom-switch-indicator"></span>
-                            </label>
-                        </li>
-                        <li>
-                            <label class="custom-switch">
-                                <span class="custom-switch-description">Box Layout</span>
-                                <input type="checkbox" name="custom-switch-checkbox" class="custom-switch-input btn-boxlayout">
-                                <span class="custom-switch-indicator"></span>
-                            </label>
-                        </li>
-                    </ul>
-                </div>
-                <hr>
-                <div class="form-group">
-                    <label class="d-block">Storage <span class="float-right">77%</span></label>
-                    <div class="progress progress-sm">
-                        <div class="progress-bar" role="progressbar" aria-valuenow="77" aria-valuemin="0" aria-valuemax="100" style="width: 77%;"></div>
-                    </div>
-                    <button type="button" class="btn btn-primary btn-block mt-3">Upgrade Storage</button>
-                </div>
-            </div>
-            <div role="tabpanel" class="tab-pane vivify fadeIn" id="activity" aria-expanded="false">
-                <ul class="new_timeline mt-3">
-                    <li>
-                        <div class="bullet pink"></div>
-                        <div class="time">11:00am</div>
-                        <div class="desc">
-                            <h3>Attendance</h3>
-                            <h4>Computer Class</h4>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="bullet pink"></div>
-                        <div class="time">11:30am</div>
-                        <div class="desc">
-                            <h3>Added an interest</h3>
-                            <h4>“Volunteer Activities”</h4>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="bullet green"></div>
-                        <div class="time">12:00pm</div>
-                        <div class="desc">
-                            <h3>Developer Team</h3>
-                            <h4>Hangouts</h4>
-                            <ul class="list-unstyled team-info margin-0 p-t-5">                                            
-                                <li><img src=".../../assets/images/xs/avatar1.jpg" alt="Avatar"></li>
-                                <li><img src="../../assets/images/xs/avatar2.jpg" alt="Avatar"></li>
-                                <li><img src="../../assets/images/xs/avatar3.jpg" alt="Avatar"></li>
-                                <li><img src="../../assets/images/xs/avatar4.jpg" alt="Avatar"></li>                                            
-                            </ul>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="bullet green"></div>
-                        <div class="time">2:00pm</div>
-                        <div class="desc">
-                            <h3>Responded to need</h3>
-                            <a href="javascript:void(0)">“In-Kind Opportunity”</a>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="bullet orange"></div>
-                        <div class="time">1:30pm</div>
-                        <div class="desc">
-                            <h3>Lunch Break</h3>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="bullet green"></div>
-                        <div class="time">2:38pm</div>
-                        <div class="desc">
-                            <h3>Finish</h3>
-                            <h4>Go to Home</h4>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-        </div>-->
     </div>
     <!-- Start Theme panel do not add in project -->
     <!-- Start Quick menu with more functio -->
@@ -276,7 +96,6 @@
                     </div>
                     <ul class="nav nav-tabs page-header-tab">
                         <li class="nav-item"><a class="nav-link active" data-toggle="tab" href="#admin-Dashboard">Dashboard</a></li>
-                        <!--<li class="nav-item"><a class="nav-link" data-toggle="tab" href="#admin-Activity">Activity</a></li>-->
                     </ul>
                 </div>
             </div>
@@ -297,7 +116,7 @@
 					<div class="col-6 col-md-4 col-xl-2">
                         <div class="card">
                             <div class="card-body">
-                                <a href="app-filemanager.html" class="my_sort_cut text-muted">
+                                <a href="courses.php" class="my_sort_cut text-muted">
                                     <i class="fa fa-folder"></i>
                                     <span>Study Materials</span>
                                 </a>
@@ -330,25 +149,6 @@
                         <div class="row clearfix row-deck">
                             <div class="col-xl-6 col-lg-6 col-md-12">
                                 <div class="card">
-                                   <!-- <div class="card-header">
-                                        <h3 class="card-title">Exam Toppers</h3>
-                                        <div class="card-options">
-                                            <a href="#" class="card-options-collapse" data-toggle="card-collapse"><i class="fe fe-chevron-up"></i></a>
-                                            <div class="item-action dropdown ml-2">
-                                                <a href="javascript:void(0)" data-toggle="dropdown"><i class="fe fe-more-vertical"></i></a>
-                                                <div class="dropdown-menu dropdown-menu-right">
-                                                    <a href="javascript:void(0)" class="dropdown-item"><i class="dropdown-icon fa fa-eye"></i> View Details </a>
-                                                    <a href="javascript:void(0)" class="dropdown-item"><i class="dropdown-icon fa fa-share-alt"></i> Share </a>
-                                                    <a href="javascript:void(0)" class="dropdown-item"><i class="dropdown-icon fa fa-cloud-download"></i> Download</a>                                            
-                                                    <div class="dropdown-divider"></div>
-                                                    <a href="javascript:void(0)" class="dropdown-item"><i class="dropdown-icon fa fa-copy"></i> Copy to</a>
-                                                    <a href="javascript:void(0)" class="dropdown-item"><i class="dropdown-icon fa fa-folder"></i> Move to</a>
-                                                    <a href="javascript:void(0)" class="dropdown-item"><i class="dropdown-icon fa fa-edit"></i> Rename</a>
-                                                    <a href="javascript:void(0)" class="dropdown-item"><i class="dropdown-icon fa fa-trash"></i> Delete</a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>-->
                                     <div class="table-responsive" style="height: 310px;">
                                         <table class="table card-table table-vcenter text-nowrap table-striped mb-0">
                                             <tbody>
