fba8308d378d262eb9d7566d86e458643a502d19
Kishan Nayak <35322004+kishannk11@users.noreply.github.com>
2021-07-25 00:11:22 +0530

    Update army.php

--- admin/army/army.php
+++ admin/army/army.php
@@ -105,7 +105,7 @@
                         <div class="card">
                             <div class="card-body">
                                 <a href="app-contact.html" class="my_sort_cut text-muted">
-                                    <i class="fa fa-address-book"></i>
+                                    <i class="fa fa-book"></i>
                                     <span>Add Courses</span>
                                 </a>
                             </div>
@@ -115,7 +115,7 @@
                         <div class="card">
                             <div class="card-body ribbon">
                                 <a href="staff.html" class="my_sort_cut text-muted">
-                                    <i class="fa fa-user-circle-o"></i>
+                                    <i class="fa fa-users"></i>
                                     <span>Add Students</span>
                                 </a>
                             </div>
@@ -134,17 +134,7 @@
                     <div class="col-6 col-md-4 col-xl-2">
                         <div class="card">
                             <div class="card-body">
-                                <a href="library.html" class="my_sort_cut text-muted">
-                                    <i class="fa fa-book"></i>
-                                    <span>Library</span>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-6 col-md-4 col-xl-2">
-                        <div class="card">
-                            <div class="card-body">
-                                <a href="holiday.html" class="my_sort_cut text-muted">
+                                <a href="holiday.php" class="my_sort_cut text-muted">
                                     <i class="fa fa-bullhorn"></i>
                                     <span>Holiday</span>
                                 </a>
