88c2f9b4aa144c867eb46baefd1c0c9cfa9fc329
Kishan Nayak <35322004+kishannk11@users.noreply.github.com>
2021-07-31 15:36:51 +0530

    Update attendance.php

--- admin/army/attendance.php
+++ admin/army/attendance.php
@@ -40,7 +40,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <link rel="icon" href="favicon.ico" type="image/x-icon"/>
-<title>:: Ericsson :: Attendance</title>
+<title>MyNCC : Admin</title>
 
 <!-- Bootstrap Core and vandor -->
 <link rel="stylesheet" href="../../assets/plugins/bootstrap/css/bootstrap.min.css" />
@@ -62,19 +62,12 @@
     <div id="header_top" class="header_top">
         <div class="container">
             <div class="hleft">
-                <a class="header-brand" href="index.html"><i class="fa fa-graduation-cap brand-logo"></i></a>
+                <a class="header-brand" href="army.php"><i class="fa fa-graduation-cap brand-logo"></i></a>
                 <div class="dropdown">
                     <a href="javascript:void(0)" class="nav-link icon menu_toggle"><i class="fe fe-align-center"></i></a>
-                    <a href="page-search.html" class="nav-link icon"><i class="fe fe-search" data-toggle="tooltip" data-placement="right" title="Search..."></i></a>
-                    <a href="app-email.html"  class="nav-link icon app_inbox"><i class="fe fe-inbox" data-toggle="tooltip" data-placement="right" title="Inbox"></i></a>
-                    <a href="app-filemanager.html"  class="nav-link icon app_file xs-hide"><i class="fe fe-folder" data-toggle="tooltip" data-placement="right" title="File Manager"></i></a>
-                    <a href="app-social.html"  class="nav-link icon xs-hide"><i class="fe fe-share-2" data-toggle="tooltip" data-placement="right" title="Social Media"></i></a>
-                    <a href="javascript:void(0)" class="nav-link icon theme_btn"><i class="fe fe-feather"></i></a>
-                    <a href="javascript:void(0)" class="nav-link icon settingbar"><i class="fe fe-settings"></i></a>
                 </div>
             </div>
             <div class="hright">
-                <a href="javascript:void(0)" class="nav-link icon right_tab"><i class="fe fe-align-right"></i></a>
                 <a href="logout.php" class="nav-link icon settingbar"><i class="fe fe-power"></i></a>                
             </div>
         </div>
@@ -84,8 +77,8 @@
     <!-- Start Theme panel do not add in project -->
     
     <!-- Start Main leftbar navigation -->
-        <div id="left-sidebar" class="sidebar">
-        <h5 class="brand-name">Ericsson<a href="javascript:void(0)" class="menu_option float-right"><i class="icon-grid font-16" data-toggle="tooltip" data-placement="left" title="Grid & List Toggle"></i></a></h5>
+    <div id="left-sidebar" class="sidebar">
+        <h5 class="brand-name">MyNCC<a href="javascript:void(0)" class="menu_option float-right"><i class="icon-grid font-16" data-toggle="tooltip" data-placement="left" title="Grid & List Toggle"></i></a></h5>
         <ul class="nav nav-tabs">
             <li class="nav-item"><a class="nav-link active" data-toggle="tab" href="#menu-admin">Admin</a></li>
         </ul>
@@ -93,12 +86,12 @@
             <div class="tab-pane fade show active" id="menu-uni" role="tabpanel">
                 <nav class="sidebar-nav">
                     <ul class="metismenu">
-						<li class="active"><a href="army.php"><i class="fa fa-dashboard"></i><span>Dashboard</span></a></li>
-                        <li><a href="payments.php"><i class="fa fa-credit-card"></i><span>Payments</span></a></li>
-                        <li><a href="add_course.php"><i class="fa fa-graduation-cap"></i><span>Add Courses</span></a></li>
-                        <li><a href="taskboard.php"><i class="fa fa-list-ul"></i><span>Taskboard</span></a></li>
-                        <li><a href="addstudent.php"><i class="fa fa-bed"></i><span>Add Student</span></a></li>
-                        <li><a href="attendance.php"><i class="fa fa-calendar-check-o"></i><span>Attendance</span></a></li>
+						<li><a href="army.php"><i class="fa fa-dashboard"></i><span>Dashboard</span></a></li>
+                        <li><a href="student.php"><i class="fa fa-graduation-cap"></></i><span>Students</span></a></li>
+                        <li><a href="add_course.php"><i class="fa fa-book"></i><span>Add Courses</span></a></li>
+                        <li><a href="addstudent.php"><i class="fa fa-users"></i><span>Add Student</span></a></li>
+                        <li class="active"><a href="attendance.php"><i class="fa fa-calendar-check-o"></i><span>Attendance</span></a></li>
+						<li><a href="events.php"><i class="fa fa-bullhorn"></i><span>Events</span></a></li>
                     </ul>
                 </nav>
             </div>
@@ -112,11 +105,13 @@
         <!-- Start Page title and tab -->
         <div class="section-body">
             <div class="container-fluid">
-                        
                 <div class="d-flex justify-content-between align-items-center ">
                     <div class="header-action">
                         <h1 class="page-title">Attendance</h1>
-
+                        <ol class="breadcrumb page-breadcrumb">
+                            <li class="breadcrumb-item"><a href="#">MyNCC</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Attendance</li>
+                        </ol>
                     </div>
                     <a href="view_at.php" class="btn btn-info btn-sm">View All Attendance</a>
                 </div>
@@ -138,20 +133,20 @@
                             
                             <div class="table-responsive"> 
                             <?php if($flag) {?>
-                        <div class="alert alert-success">
-                        <Strong>Attendance added Succesfully</strong>
-                        </div>
-                        <?php } ?>
-                        <?php if($dat) {?>
-                        <div class="alert alert-danger">
-                        <Strong>Cannot Use Future Date</strong>
-                        </div>
-                        <?php } ?>
-                        <?php if($count){?>
-                        <div class="alert alert-danger">
-                        <Strong>Attendance already marked</strong>
-                        </div>
-                        <?php } ?>
+								<div class="alert alert-success">
+									<Strong>Attendance added Succesfully</strong>
+								</div>
+							<?php } ?>
+							<?php if($dat) {?>
+								<div class="alert alert-danger">
+									<Strong>Cannot Use Future Date</strong>
+								</div>
+							<?php } ?>
+							<?php if($count){?>
+								<div class="alert alert-danger">
+									<Strong>Attendance already marked</strong>
+								</div>
+							<?php } ?>
                                                          
                                 <table class="table table-sm table-hover table-striped table-vcenter mb-0 text-nowrap">
                                     <thead>
@@ -159,7 +154,7 @@
                                         <th>Serial number</th>
                                        <th>Roll Number</th>
                                         <th>Name</th> 
-                                       <th>isPresent</th>
+                                       <th>is Present</th>
                                      </tr>
                                         <?php 
                                         include('conn.php');
@@ -190,12 +185,13 @@
                                     </tr>
                                     </tbody>
                                 </table>
-                                
                             </div>
                         </div>
                     </div>
                 </div>
-               <button type="submit" class="btn btn-primary btn-block" name="submit">SAVE</button>
+                <div>
+                    <center><button type="submit" class="btn btn-primary" name="submit">Submit</button></center>
+                </div>
             </div>
         </div>
         
@@ -206,14 +202,7 @@
                 <div class="container-fluid">
                     <div class="row">
                         <div class="col-md-6 col-sm-12">
-                            Copyright © 2019 <a href="https://themeforest.net/user/puffintheme/portfolio">PuffinTheme</a>.
-                        </div>
-                        <div class="col-md-6 col-sm-12 text-md-right">
-                            <ul class="list-inline mb-0">
-                                <li class="list-inline-item"><a href="../../doc/index.html">Documentation</a></li>
-                                <li class="list-inline-item"><a href="javascript:void(0)">FAQ</a></li>
-                                <li class="list-inline-item"><a href="javascript:void(0)" class="btn btn-outline-primary btn-sm">Buy Now</a></li>
-                            </ul>
+                            Copyright © 2021 | Phoenix Studios | <a href="https://kishannayak.000webhostapp.com/">Kishan Nayak</a>.
                         </div>
                     </div>
                 </div>
