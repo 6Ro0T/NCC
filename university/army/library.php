a127ef7e226bacc909a68dc6b9d597711fe9bc3b
Kishan Nayak <35322004+kishannk11@users.noreply.github.com>
2021-08-08 17:49:50 +0530

    Update library.php

--- university/army/library.php
+++ university/army/library.php
@@ -98,149 +98,34 @@
                                        <thead>
                                              <tr>
                                                 <!-- <th>#</th> -->
-                                                <th>Title</th>
+                                                <th>SR No</th>
                                                 <th>Subject</th>
                                                 <th>Department</th>
                                                 <th>Year</th>
-                                                <th>Type</th>
-                                                <th>Status</th>
+                                                <th>View</th>
                                             </tr>
                                         </thead>
                                         <tbody>
                                             <tr>
                                                 <td>1</td>
-                                                <td>Management Basics</td>
-                                                <td>Designing</td>
-                                                <td>Architecture</td>
-                                                <td>Second Year</td>
-                                                <td>CD</td>
-                                                <td>Out of Stock</td>
+                                                <td>SD_SW_Common Handbook</td>
+                                                <td>Common</td>
+                                                <td>All Year</td>
+                                                <td><a href="https://pdfhost.io/v/X.hMeTxfg_sdswcommonsubject.pdf">View Here</a></td>
                                             </tr>
                                             <tr>
                                                 <td>2</td>
-                                                <td>Java Black Book</td>
-                                                <td>Graphics</td>
-                                                <td>MBA</td>
-                                                <td>Third Year</td>
-                                                <td>DVD</td>
-                                                <td>Out of Stock</td>
+                                                <td>SD_SW_Army Cadet Handbook</td>
+                                                <td>Army</td>
+                                                <td>All Year</td>
+                                                <td>View Here</td>
                                             </tr>
                                             <tr>
                                                 <td>3</td>
-                                                <td>Parallel Computing</td>
-                                                <td>Politics</td>
-                                                <td>Architecture</td>
-                                                <td>Third Year</td>
-                                                <td>Newspaper</td>
-                                                <td>Out of Stock</td>
-                                            </tr>
-                                            <tr>
-                                                <td>4</td>
-                                                <td>Networking</td>
-                                                <td>Computer</td>
-                                                <td>MBA</td>
-                                                <td>Last Year</td>
-                                                <td>Newspaper</td>
-                                                <td>In Stock</td>
-                                            </tr>
-                                            <tr>
-                                                <td>5</td>
-                                                <td>Web Programming</td>
-                                                <td>Mechanics</td>
-                                                <td>Computer</td>
-                                                <td>Third Year</td>
-                                                <td>Book</td>
-                                                <td>In Stock</td>
-                                            </tr>
-                                            <tr>
-                                                <td>6</td>
-                                                <td>Designing</td>
-                                                <td>Mechanics</td>
-                                                <td>Computer</td>
-                                                <td>Third Year</td>
-                                                <td>Newspaper</td>
-                                                <td>Out of Stock</td>
-                                            </tr>
-                                            <tr>
-                                                <td>7</td>
-                                                <td>Mechanics</td>
-                                                <td>Computer</td>
-                                                <td>Computer Engineering</td>
-                                                <td>Second Year</td>
-                                                <td>DVD</td>
-                                                <td>Out of Stock</td>
-                                            </tr>
-                                            <tr>
-                                                <td>8</td>
-                                                <td>Computer Fundamental</td>
-                                                <td>Animation</td>
-                                                <td>Mechanical</td>
-                                                <td>Third Year</td>
-                                                <td>CD</td>
-                                                <td>In Stock</td>
-                                            </tr>
-                                            <tr>
-                                                <td>9</td>
-                                                <td>Time History</td>
-                                                <td>Management</td>
-                                                <td>Mechanical</td>
-                                                <td>Third Year</td>
-                                                <td>Newspaper</td>
-                                                <td>Out of Stock</td>
-                                            </tr>
-                                            <tr>
-                                                <td>10</td>
-                                                <td>Politics</td>
-                                                <td>Designing</td>
-                                                <td>Mechanical</td>
-                                                <td>Second Year</td>
-                                                <td>Newspaper</td>
-                                                <td>Out of Stock</td>
-                                            </tr>
-                                            <tr>
-                                                <td>11</td>
-                                                <td>Designing</td>
-                                                <td>Mechanics</td>
-                                                <td>Computer</td>
-                                                <td>Third Year</td>
-                                                <td>Newspaper</td>
-                                                <td>Out of Stock</td>
-                                            </tr>
-                                            <tr>
-                                                <td>12</td>
-                                                <td>Mechanics</td>
-                                                <td>Computer</td>
-                                                <td>Computer Engineering</td>
-                                                <td>Second Year</td>
-                                                <td>DVD</td>
-                                                <td>Out of Stock</td>
-                                            </tr>
-                                            <tr>
-                                                <td>13</td>
-                                                <td>Computer Fundamental</td>
-                                                <td>Animation</td>
-                                                <td>Mechanical</td>
-                                                <td>Third Year</td>
-                                                <td>CD</td>
-                                                <td>In Stock</td>
-                                            </tr>
-                                            <tr>
-                                                <td>14</td>
-                                                <td>Time History</td>
-                                                <td>Management</td>
-                                                <td>Mechanical</td>
-                                                <td>Third Year</td>
-                                                <td>Newspaper</td>
-                                                <td>Out of Stock</td>
-                                            </tr>
-                                            <tr>
-                                                <td>15</td>
-                                                <td>Politics</td>
-                                                <td>Designing</td>
-                                                <td>Mechanical</td>
-                                                <td>Second Year</td>
-                                                <td>Newspaper</td>
-                                                <td>Out of Stock</td>
+                                                <td>SD_SW_Army Cadet Lab Manual</td>
+                                                <td>Army</td>
+                                                <td>All Year</td>
+                                                <td>View Here</td>
                                             </tr>
                                         </tbody>
                                     </table>
