2d40f7b4c388edbdef173af5a637560a084e190d
Kishan Nayak <35322004+kishannk11@users.noreply.github.com>
2021-09-04 18:22:52 +0530

    Update library.php

--- university/airforce/library.php
+++ university/airforce/library.php
@@ -35,7 +35,7 @@
     <div id="header_top" class="header_top">
         <div class="container">
             <div class="hleft">
-                <a class="header-brand" href="army.php"><i class="fa fa-graduation-cap brand-logo"></i></a>
+                <a class="header-brand" href="airforce.php"><i class="fa fa-graduation-cap brand-logo"></i></a>
                 <div class="dropdown">
                     <a href="javascript:void(0)" class="nav-link icon menu_toggle"><i class="fe fe-align-center"></i></a>
                 </div>
@@ -55,7 +55,7 @@
             <div class="tab-pane fade show active" id="menu-uni" role="tabpanel">
                 <nav class="sidebar-nav">
                     <ul class="metismenu">
-                        <li><a href="army.php"><i class="fa fa-dashboard"></i><span>Dashboard</span></a></li>
+                        <li><a href="airforce.php"><i class="fa fa-dashboard"></i><span>Dashboard</span></a></li>
                         <li><a href="viewat.php"><i class="fa fa-black-tie"></i><span>View Attendance</span></a></li>
                         <li><a href="staff.php"><i class="fa fa-users"></i><span>Staff</span></a></li>
                         <li><a href="courses.php"><i class="fa fa-graduation-cap"></i><span>Classes</span></a></li>                        
@@ -119,13 +119,6 @@
                                                 <td>Army</td>
                                                 <td>All Year</td>
                                                 <td><a href="https://pdfhost.io/v/3cS5kjUgO_Army_Cadet_Handbook.pdf" target="_blank">View Here</a></td>
-                                            </tr>
-                                            <tr>
-                                                <td>3</td>
-                                                <td>SD_SW_Army Cadet Lab Manual</td>
-                                                <td>Army</td>
-                                                <td>All Year</td>
-                                                <td><a href="#">View Here</a></td>
                                             </tr>
                                         </tbody>
                                     </table>
